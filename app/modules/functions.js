647c4b96fe334362fc15ca9a1c01fd43e42c7973
Артур <artur.archevodov@gmail.com>
2023-08-18 02:29:06 +0300

    Правки

--- app/modules/functions.js
+++ app/modules/functions.js
@@ -3,35 +3,32 @@ import axios from 'axios'
 export const name = 'functions'
 
 export async function censureCheck(context) {
-    let result = await axios.get(process.env.CENSURE_QUERY_URL + context.message.text)
-    .then((response) => {
-        return true
-    })
-    .catch((error) => {
-        console.log(error)
-        return false
-    })
-    return result
+    return await axios.get(process.env.CENSURE_QUERY_URL + context.message.text)
+        .then((response) => {
+            return true
+        })
+        .catch((error) => {
+            console.log(error)
+            return false
+        })
 }
 
 export async function addScore(context) {
-    let result = await axios.get(process.env.SET_SCORE_QUERY_URL + context.message.from.id)
-    .then((response) => {
-        return true
-    })
-    .catch((error) => {
-        return false
-    })
-    return result
+    return await axios.get(process.env.SET_SCORE_QUERY_URL + context.message.from.id)
+        .then((response) => {
+            return true
+        })
+        .catch((error) => {
+            return false
+        })
 }
 
 export async function getScore(context) {
-    let result = await axios.get(process.env.GET_SCORE_QUERY_URL + context.message.from.id)
-    .then((response) => {
-        return response.data
-    })
-    .catch((error) => {
-        return false
-    })
-    return result
+    return await axios.get(process.env.GET_SCORE_QUERY_URL + context.message.from.id)
+        .then((response) => {
+            return response.data
+        })
+        .catch((error) => {
+            return false
+        })
 }
